0b43ed4da3753751c8dcf5fd59b7bbcf388b2be4
Whackamole5 <michael.legosrfun@gmail.com>
2025-03-26 19:39:45 -0400

    Update StudentTests.java

--- StudentTests.java
+++ StudentTests.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import listClasses.BasicLinkedList;
-import listClasses.SortedLinkedList;
 
 /**
  * 
@@ -16,109 +15,62 @@ import listClasses.SortedLinkedList;
  *
  */
 public class StudentTests {
+	
+	// Tests for BasicLinkedList<T>
 
+	@Test // tests functions with a null list ensuring that they work in an empty list scenario.
+	public void NullTester() {
+		BasicLinkedList<String> test = new BasicLinkedList<String>();
+		
+	}
+	
+	// tests addToFront, addToEnd, getSize, getFirst, and getLast with a small linked list.
+	// checks every time a node is added to the LinkedList.
+	@Test 
+	public void addingTester() {
+		BasicLinkedList<String> test = new BasicLinkedList<String>();
+		
+		assertTrue(test.getSize()==0);
+		assertTrue(test.getFirst()==null);
+		assertTrue(test.getLast()==null);
+		test.addToFront("one");
+		assertTrue(test.getSize()==1);
+		assertTrue(test.getFirst().equals("one"));
+		assertTrue(test.getLast().equals("one"));
+		test.addToEnd("two");
+		assertTrue(test.getSize()==2);
+		assertTrue(test.getFirst().equals("one"));
+		assertTrue(test.getLast().equals("two"));
+		test.addToFront("three");
+		assertTrue(test.getSize()==3);
+		assertTrue(test.getFirst().equals("three"));
+		assertTrue(test.getLast().equals("two"));
+		test.addToEnd("four");
+		assertTrue(test.getSize()==4);
+		assertTrue(test.getFirst().equals("three"));
+		assertTrue(test.getLast().equals("four"));
+		
+	}
+	
+	@Test // tests the remove function
+	public void removeTester() {
+		BasicLinkedList<String> test = new BasicLinkedList<String>();
+		
+		test.addToFront("one");
+		test.addToEnd("two");
+		test.addToEnd("three");
+		test.addToFront("four");
+		test.addToFront("three");
+		
+		test.remove("three", String.CASE_INSENSITIVE_ORDER);
+		
+	}
+	
+	@Test // tests retrieveFirstElement and retrieveLastElement
+	public void retrieveTester() {
+		BasicLinkedList<String> test = new BasicLinkedList<String>();
+	}
+	
+	// Tests for SortedLinkedList<T>
 
-	/**
-	@Test
-	public void addingTest() {
-		BasicLinkedList<String> tester = new BasicLinkedList<String>();
-		
-		tester.addToEnd("one");
-		System.out.println(tester.getFirst());
-		System.out.println(tester.getLast());
-		System.out.println(tester.getSize());
-		tester.addToFront("two");
-		System.out.println(tester.getFirst());
-		System.out.println(tester.getLast());
-		System.out.println(tester.getSize());
-		tester.addToFront("three");
-		System.out.println(tester.getFirst());
-		System.out.println(tester.getLast());
-		System.out.println(tester.getSize());
-		tester.addToEnd("four");
-		System.out.println(tester.getFirst());
-		System.out.println(tester.getLast());
-		System.out.println(tester.getSize());
-	}
-	**/
-	
-	/**
-	@Test
-	public void basicRemovalTest() {
-		BasicLinkedList<String> tester = new BasicLinkedList<String>();
-		
-		tester.addToEnd("one");
-		tester.addToFront("two");
-		System.out.println(tester.getFirst());
-		System.out.println(tester.getLast());
-		System.out.println(tester.getSize());
-		tester.addToFront("three");
-		tester.addToFront("four");
-		tester.addToFront("five");
-		tester.remove("one", String.CASE_INSENSITIVE_ORDER);
-	}
-	**/
-	
-	
-	@Test
-	public void reverseListTest() {
-		BasicLinkedList<String> tester = new BasicLinkedList<String>();
-		
-		tester.addToEnd("one");
-		tester.addToFront("two");
-		tester.addToFront("three");
-		tester.addToFront("four");
-		tester.addToFront("five");
-		tester.addToEnd("six");
-		
-		BasicLinkedList<String> revTester = tester.getReverseList();
-		
-		System.out.println(revTester.getFirst());
-		System.out.println(revTester.getLast());
-		
-	}
-	
-	/**
-	@Test
-	public void reverseArrayListTest() {
-		BasicLinkedList<String> tester = new BasicLinkedList<String>();
-		
-		tester.addToEnd("one");
-		tester.addToFront("two");
-		tester.addToFront("three");
-		tester.addToFront("four");
-		tester.addToFront("five");
-		tester.addToEnd("six");
-		
-		System.out.println(tester.getReverseArrayList());
-	}
-	**/
-	
-	public static void main(String[] args) {
-		BasicLinkedList<String> basicList = new BasicLinkedList<String>();
-
-		basicList.addToEnd("Red").addToFront("Yellow").addToFront("Blue");
-		System.out.println("First: " + basicList.getFirst());
-		System.out.println("Last: " + basicList.getLast());
-		System.out.println("Size: " + basicList.getSize());
-		System.out.println("Retrieve First: " + basicList.retrieveFirstElement());
-		System.out.println("Retrieve Last: " + basicList.retrieveLastElement());
-		System.out.println("Removing Red");
-		basicList.remove("Red", String.CASE_INSENSITIVE_ORDER);
-		System.out.print("Iteration: ");
-		for (String entry : basicList) {
-			System.out.print(entry + " ");
-		}
-
-		System.out.println("Iteration Worked");
-		
-		SortedLinkedList<String> sortedList = new SortedLinkedList<String>(String.CASE_INSENSITIVE_ORDER);
-		System.out.print("\n\nIteration (for sorted list): ");
-		for (String entry : sortedList) {
-			System.out.print(entry + " ");
-		}
-		sortedList.remove("Red");
-		System.out.print("\nAfter remove in sorted list first is: ");
-		System.out.println(sortedList.getFirst());
-	}
 }
