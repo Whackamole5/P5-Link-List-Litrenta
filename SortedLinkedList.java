ac79b28b279a38b2860cd3f953b71164054e9403
Whackamole5 <michael.legosrfun@gmail.com>
2025-03-26 19:40:27 -0400

    Update SortedLinkedList.java

--- SortedLinkedList.java
+++ SortedLinkedList.java
@@ -1,60 +1,60 @@
+
 package listClasses;
 
 import java.util.*;
-
 
 /**
  * Implements a generic sorted list using a provided Comparator. It extends
  * BasicLinkedList class.
  * 
- *  @author Dept of Computer Science, UMCP
- *  
+ * @author Dept of Computer Science, UMCP
+ * 
  */
 
 public class SortedLinkedList<T> extends BasicLinkedList<T> {
 	private Comparator<T> comparator;
-	
+
 	// SortedLinkedList Constructor
 	public SortedLinkedList(Comparator<T> comparator) {
 		super();
 		this.comparator = comparator;
 	}
-	
+
 	// adds an element T to a sorted linked list using the comparator initial variable
 	public SortedLinkedList<T> add(T element) {
 		Node placement = this.head;
 		Node input = new Node(element);
-		if (head == null) {
-			super.addToFront(element);
+		if (head == null) { // if the head is null
+			input.next = null;
+			head = input;
 			return this;
 		}
-		if (comparator.compare(element, placement.data)==1) {
+		if (comparator.compare(element, placement.data) >= 1) {
 			placement = placement.next;
-		}
-		else {
+		} else {
 			placement.next = input;
 			return this;
 		}
-		
+
 		return this;
 	}
-	
-	// removes element T from a SortedLinkedList using the super remove 
+
+	// removes element T from a SortedLinkedList using the super remove
 	public SortedLinkedList<T> remove(T element) {
 		super.remove(element, comparator);
 		return this;
 	}
-	
+
 	@Override
 	// Overrides the addToFront method of BasicLinkedList since this list is sorted
 	public BasicLinkedList<T> addToFront(T data) {
 		throw new UnsupportedOperationException("Invalid operatoin for sorted list.");
 	}
-	
+
 	@Override
 	// overrides the addToEnd method of BasicLinkedList since this list is sorted
 	public BasicLinkedList<T> addToEnd(T data) {
 		throw new UnsupportedOperationException("Invalid operatoin for sorted list.");
 	}
-	
+
 }
