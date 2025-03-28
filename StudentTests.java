b3715d003d864c901db8c58b9967670365da77f2
Whackamole5 <michael.legosrfun@gmail.com>
2025-03-27 21:49:40 -0400

    Update StudentTests.java

--- StudentTests.java
+++ StudentTests.java
@@ -2,6 +2,8 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import listClasses.BasicLinkedList;
@@ -28,6 +30,7 @@ public class StudentTests {
 	
 	// tests addToFront, addToEnd, getSize, getFirst, and getLast with a small linked list.
 	// checks every time a node is added to the LinkedList.
+	// all work
 	@Test 
 	public void addingTester() {
 		BasicLinkedList<String> test = new BasicLinkedList<String>();
@@ -54,10 +57,10 @@ public class StudentTests {
 		
 	}
 	
-	@Test // tests the remove function
+	// tests the remove function. works in scenario
+	@Test 
 	public void removeTester() {
 		BasicLinkedList<String> test = new BasicLinkedList<String>();
-		BasicLinkedList<String> after = new BasicLinkedList<String>();
 		
 		test.addToFront("three");
 		test.addToFront("one");
@@ -68,21 +71,91 @@ public class StudentTests {
 		
 		test.remove("three", String.CASE_INSENSITIVE_ORDER);
 		
-		after.addToFront("two");
-		after.addToFront("one");
-		after.addToFront("four");
-		
-		System.out.println(test.getSize());
+		assertTrue(test.getSize()==3);
 		assertTrue(test.getFirst().equals("four"));
 		assertTrue(test.getLast().equals("two"));
 		
 	}
 	
-	@Test // tests retrieveFirstElement and retrieveLastElement
+	// tests retrieveFirstElement and retrieveLastElement
+	@Test
 	public void retrieveTester() {
 		BasicLinkedList<String> test = new BasicLinkedList<String>();
+		test.addToFront("three");
+		test.addToFront("one");
+		test.addToEnd("two");
+		
+		assertTrue(test.retrieveFirstElement().equals("one"));
+		assertTrue(test.getSize()==2);
+		assertTrue(test.getFirst().equals("three"));
+		assertTrue(test.getLast().equals("two"));
+		
+		assertTrue(test.retrieveLastElement().equals("two"));
+		assertTrue(test.getSize()==1);
+		assertTrue(test.getFirst().equals("three"));
+		assertTrue(test.getLast().equals("three")); // error/fails
+	}
+	
+	// tests the iterator
+	@Test
+	public void iteratorTester() {
+		BasicLinkedList<String> test = new BasicLinkedList<String>();
+		test.addToFront("three");
+		test.addToFront("one");
+		test.addToEnd("two");
+		
+		ArrayList<String> testArr = new ArrayList<String>();
+		testArr.add("one");
+		testArr.add("three");
+		testArr.add("two");		
+		
+		int index = 0;
+		for (String s : test) {
+			assertTrue(s.equals(testArr.get(index)));
+			index++;
+		}
+		
+	}
+	
+	@Test // tests getReverseList and getReverseArrayList
+	public void reverseTester() {
+		BasicLinkedList<String> test = new BasicLinkedList<String>();
+		test.addToFront("five");
+		test.addToFront("one");
+		test.addToEnd("two");
+		test.addToEnd("three");
+		test.addToFront("four");
+		test.addToFront("six");
+		
+		ArrayList<String> wantedArr = new ArrayList<String>();
+		wantedArr.add("three");
+		wantedArr.add("two");
+		wantedArr.add("five");
+		wantedArr.add("one");
+		wantedArr.add("four");
+		wantedArr.add("six");
+		
+		ArrayList<String> revArr = test.getReverseArrayList();
+		
+		int index = 0;
+		for (String s : revArr) {
+			assertTrue(s.equals(wantedArr.get(index)));
+			index++;
+		}
+		
+		BasicLinkedList<String> revList = test.getReverseList();
+		index = 0;
+		for (String s : revList) {
+			assertTrue(s.equals(wantedArr.get(index)));
+			index++;
+		}
+		
 	}
 	
 	// Tests for SortedLinkedList<T>
-
+	
+	@Test
+	public void sortedAdd() {
+		
+	}
 }
